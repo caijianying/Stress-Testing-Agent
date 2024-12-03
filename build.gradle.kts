8b8b7573ee90cf73375d04487111287056194b48
caijy <c18268151702@163.com>
2024-12-03 17:56:53 +0800

    feat: 支持java17

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     testCompileOnly("${versions["junit"]}")
     testImplementation("${versions["junit.jupiter.api"]}")
     testImplementation("${versions["junit.jupiter.engine"]}")
-    // 兼容 mybatis3.5.9  @see https://baomidou.com/getting-started/install
+    // 兼容JDK17  @see https://baomidou.com/getting-started/install
     implementation("${versions["sqlParser"]}")
 
 
