89f29beab1cbc6f7864f4cbe469ebff864e61a96
caijy <c18268151702@163.com>
2025-02-11 17:51:11 +0800

    更改压测标

--- agent-core/src/main/java/com/xiaobaicai/agent/core/constants/StressTestingConstant.java
+++ agent-core/src/main/java/com/xiaobaicai/agent/core/constants/StressTestingConstant.java
@@ -7,7 +7,7 @@ package com.xiaobaicai.agent.core.constants;
  */
 public interface StressTestingConstant {
     String HEADER_NAME_STRESS_TESTING_FLAG = "pt-flag";
-    String HEADER_VALUE_STRESS_TESTING_FLAG = "pt";
+    String HEADER_VALUE_STRESS_TESTING_FLAG = "STA";
     String DATABASE_NAME_KEY = "DATABASE_NAME";
     String IN_PT_KEY = "IN_PT";
 }
